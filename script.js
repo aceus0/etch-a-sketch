ebb7c618f8e25a593ca16dc40096fe46ec0cf63a
justin <64299580+aceus0@users.noreply.github.com>
2024-04-16 23:38:25 -0400

    basic etch of sketch functionality created

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 
 
 
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
 
     let i = 0
-    while (i < 512) {
+    while (i < 256) {
     const pixel = document.createElement("div");
     pixel.style.backgroundColor = "white";
     pixel.style.height = "32px";
     pixel.style.width = "32px";
-
+    pixel.setAttribute("id", "pixel");
+ 
     container.appendChild(pixel);
     i++
     }
+
+const highlightPixel = (e) => {
+    target.style.backgroundColor = "black"
+}
+
+const pixels = document.querySelectorAll("#pixel");
+console.log(pixels);
+
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', function() {
+        this.style.backgroundColor = 'black';
+    })
+});
+
+const clearButton = document.querySelector("#clear")
+
+clearButton.addEventListener(`click`, () => {
+    pixels.style.backgroundColor = 'white';
+})
